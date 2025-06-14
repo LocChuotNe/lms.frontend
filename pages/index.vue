674e18ba2b2451532458dd88f5e8ebcb6f602c18
LocChuotNe <locphamt18@gmail.com>
2025-06-15 02:08:13 +0700

    Initial commit for frontend

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-3xl font-bold text-blue-500">Hello Tailwind + Nuxt 3!</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        './components/**/*.{js,ts,vue}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './app.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
