46f8ed43f0ea01c299283d81e034271fa90228be
LocChuotNe <locphamt18@gmail.com>
2025-06-17 23:59:55 +0700

    bug loi cai dat ant-desgint va cau hinh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-  css: ["~/assets/css/index.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+  ],
+  css: [
+    "~/assets/css/index.css"
+  ],
   postcss: {
   plugins: {
     "@tailwindcss/postcss": {},
+    '@/plugins/antd': {},
     autoprefixer: {},
   }
   }
