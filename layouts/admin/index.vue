1e9d000552b363a1c157b8726aad68a1161e766a
LocChuotNe <locphamt18@gmail.com>
2025-06-18 23:46:35 +0700

    layout

---
+++ layouts/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="admin-layout">
+    <header>Admin Header</header>
+    <aside>Sidebar quản trị</aside>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ pages/admin/user.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import UserTable from '~/components/admin/UserTable.vue'
+
+    definePageMeta({ layout: 'default' })
+</script>
+
+<template>
+  <div>
+    <h1>Quản lý người dùng</h1>
+    <UserTable />
+  </div>
+</template>
